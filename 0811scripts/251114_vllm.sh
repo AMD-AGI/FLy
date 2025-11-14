0a54da24d963d2eccfae77611c5754c858acfed3
jinze <46251666+userljz@users.noreply.github.com>
2025-11-14 15:07:40 +0800

    1114

---
+++ 0811scripts/251114_vllm.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export HF_HOME=/wekafs/jinzeli2/cache
+
+# Model paths
+llama33_70B='meta-llama/Llama-3.3-70B-Instruct'
+llama31_70B='meta-llama/Llama-3.1-70B-Instruct'
+llama31_405B='meta-llama/Llama-3.1-405B-Instruct'
+llama31_8B='meta-llama/Llama-3.1-8B-Instruct'
+
+# Target model (main model)
+target_model=${llama31_70B}
+
+# GPU configuration
+export HIP_VISIBLE_DEVICES='0'
+# export HIP_VISIBLE_DEVICES='4'
+export HF_ALLOW_CODE_EVAL=1
+
+# FLy algorithm parameters
+enable_fly=True
+fly_win_len=8
+entropy_threshold=None  # Set to a float value if needed, e.g., 0.5
+
+# Task to evaluate
+task=humaneval_instruct
+
+# Output directory
+data=251114
+
+# Run evaluation with vLLM
+python -m lm_eval \
+    --model vllm \
+    --model_args pretrained=${target_model},enable_fly=${enable_fly},fly_win_len=${fly_win_len},entropy_threshold=${entropy_threshold} \
+    --tasks ${task} \
+    --output_path ./eval_out/${data}_${task} \
+    --show_config \
+    --log_samples \
+    --batch_size 1 \
+    --apply_chat_template \
+    --fewshot_as_multiturn \
+    --confirm_run_unsafe_code \
+    &> ./output_log/${data}_${task}_vllm_FLy.log
+
